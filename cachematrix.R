44dd3dd06171cb1ab6e51f0d7493bfb74c80fbcd
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    commit function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <-function(mt=matrix()){
+  invert_mt <- NULL
+  
+  set_matrix<-function(set_mt){
+    
+    mt <<-set_mt
+    invert_mt<<-NULL
+  }
+  
+  get_matrix<-function(){
+    print(mt)
+  }
+  
+  set_invert_mt<-function(y){
+    invert_mt<<-y
+  }
+  
+  get_invert_mt<-function(){
+    print(invert_mt)
+  }
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <-function(mt,...){
+  
+  ##IF we have a data 
+  if(!(mt$get_invert_mt)){
+    print("We already have the data")
+    return (mt$get_invert_mt)
+  }
+  
+  ##IF we don't have cached data
+  mt$set_invert_mt( solve((mt$get_matrix()), ...))
+  
 }
+
